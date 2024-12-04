4130e1ff9c123ad143e5f477d33a095c11ab5888
Mathurin Chritin <mathurin.chritin@epfl.ch>
2024-12-04 13:53:18 +0000

    devcontainer configuration

---
+++ .devcontainer/post-install.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+sudo apt-get update
+sudo apt-get install -y ruby-full build-essential zlib1g-dev
+
+echo '# Install Ruby Gems to ~/gems' >> ~/.bashrc
+echo 'export GEM_HOME="$HOME/gems"' >> ~/.bashrc
+echo 'export PATH="$HOME/gems/bin:$PATH"' >> ~/.bashrc
+source ~/.bashrc
+
+gem install jekyll bundler
